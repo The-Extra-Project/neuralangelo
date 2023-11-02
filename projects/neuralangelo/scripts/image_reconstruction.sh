6db84bf8a5fddf1cb855335c39fed37c766d5f01
Dhruv Malik <malikdhruv1994@gmail.com>
2023-11-02 12:48:19 +0000

    setting up the training pipeline complete.

--- projects/neuralangelo/scripts/image_reconstruction.sh
+++ projects/neuralangelo/scripts/image_reconstruction.sh
@@ -10,3 +10,33 @@ DATA_PATH=./datasets/${SEQUENCE}_ds${DOWNSAMPLE_RATE}
 bash projects/neuralangelo/scripts/run_colmap.sh ${DATA_PATH}
 
 
+EXPERIMENT=$SEQUENCE
+GROUP=example_group
+NAME=$SEQUENCE
+CONFIG=projects/neuralangelo/configs/custom/${EXPERIMENT}.yaml
+GPUS=1  
+
+## running the training on the mesh algorithm
+torchrun --nproc_per_node=${GPUS} train.py \
+         --logdir=logs/${GROUP}/${NAME} \
+	 --config=${CONFIG} \
+         --show_pbar
+
+
+## running the isometric reconstruction
+
+
+
+CHECKPOINT=logs/${GROUP}/${NAME}/xxx.pt
+OUTPUT_MESH=xxx.ply
+CONFIG=logs/${GROUP}/${NAME}/config.yaml
+RESOLUTION=2048
+BLOCK_RES=128
+GPUS=1  # use >1 for multi-GPU mesh extraction
+torchrun --nproc_per_node=${GPUS} projects/neuralangelo/scripts/extract_mesh.py \
+    --config=${CONFIG} \
+    --checkpoint=${CHECKPOINT} \
+    --output_file=${OUTPUT_MESH} \
+    --resolution=${RESOLUTION} \
+    --block_res=${BLOCK_RES}
+
